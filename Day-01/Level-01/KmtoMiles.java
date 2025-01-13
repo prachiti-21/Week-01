7b7de0f2d2b141703a865383cd33ed493737b8d9
Prachiti <prachitiagarkar1@gmail.com>
2025-01-13 10:34:35 +0530

    Covert kilometers to miles 2

---
+++ Day-01/Level-01/KmtoMiles.java
@@ -0,0 +1,12 @@
+class KmtoMiles{
+public static void main(String args[]){
+  //initializing distance variable
+  double km=10.8;
+
+ //covert into miles
+double miles= km/1.6;
+
+ //display
+System.out.println("The distance "+km +"km in miles is "+miles);
+}
+}
